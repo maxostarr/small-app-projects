ef410e8ebddd7d2790aba47f12fa7a6a014ec7e2
Max Starr <manmon42@gmail.com>
2020-03-16 15:46:22 -0700

    border radius testing

--- apps/border-radius-preview/src/App.test.tsx
+++ apps/border-radius-preview/src/App.test.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "../utils.test";
+import App from "./App";
+import { getByTestId, fireEvent } from "@testing-library/react";
 
-test('renders learn react link', () => {
+test("renders", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = getByText(/Border Radius Preview!/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test("changes border radius", () => {
+  const { getByTestId } = render(<App />);
+  const inputElement = getByTestId("topLeft");
+  const cardElement = getByTestId("card");
+  expect(cardElement).toHaveStyle(
+    "border-radius: 10% 10% 10% 10% / 10% 10% 10% 10%"
+  );
+  fireEvent.change(inputElement, { target: { value: "20" } });
+  expect(cardElement).toHaveStyle(
+    "border-radius: 20% 10% 10% 10% / 10% 10% 10% 10%"
+  );
+});
+
+test("changes the css display", () => {
+  const { getByTestId } = render(<App />);
+  const inputElement = getByTestId("topLeft");
+  const outputElement = getByTestId("output");
+  expect(outputElement).toHaveTextContent(
+    "border-radius: 10% 10% 10% 10% / 10% 10% 10% 10%"
+  );
+  fireEvent.change(inputElement, { target: { value: "20" } });
+  expect(outputElement).toHaveTextContent(
+    "border-radius: 20% 10% 10% 10% / 10% 10% 10% 10%"
+  );
 });
