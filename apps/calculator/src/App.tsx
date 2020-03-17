1e255864340bb48c9c0e8b6bd4e1ef2ceb1b6748
Max Starr <manmon42@gmail.com>
2020-03-16 17:17:28 -0700

    standard color

--- apps/calculator/src/App.tsx
+++ apps/calculator/src/App.tsx
@@ -3,120 +3,120 @@ import { Card, GridContainer, GridItem } from "components/index";
 import "./App.css";
 
 const buttons = [
-  { label: "", columns: "span 4", rows: "", color: "#111" },
+  { label: "", columns: "span 4", rows: "", color: "#111111" },
   {
     label: "AC",
     columns: "",
     rows: "",
-    color: "rgba(202,202,204,1)"
+    color: "#cacacc"
   },
   {
     label: "%",
     columns: "",
     rows: "",
-    color: "rgba(202,202,204,1)"
+    color: "#cacacc"
   },
   {
     label: "±",
     columns: "",
     rows: "",
-    color: "rgba(202,202,204,1)"
+    color: "#cacacc"
   },
   {
     label: "/",
     columns: "",
     rows: "",
-    color: "rgba(252,156,23,1)"
+    color: "#fc9c17"
   },
   {
     label: "7",
     columns: "",
     rows: "",
-    color: " #777"
+    color: "#777777"
   },
   {
     label: "8",
     columns: "",
     rows: "",
-    color: " #777"
+    color: "#777777"
   },
   {
     label: "9",
     columns: "",
     rows: "",
-    color: " #777"
+    color: "#777777"
   },
   {
     label: "*",
     columns: "",
     rows: "",
-    color: "rgba(252,156,23,1)"
+    color: "#fc9c17"
   },
   {
     label: "4",
     columns: "",
     rows: "",
-    color: " #777"
+    color: "#777777"
   },
   {
     label: "5",
     columns: "",
     rows: "",
-    color: " #777"
+    color: "#777777"
   },
   {
     label: "6",
     columns: "",
     rows: "",
-    color: " #777"
+    color: "#777777"
   },
   {
     label: "-",
     columns: "",
     rows: "",
-    color: "rgba(252,156,23,1)"
+    color: "#fc9c17"
   },
   {
     label: "1",
     columns: "",
     rows: "",
-    color: " #777"
+    color: "#777777"
   },
   {
     label: "2",
     columns: "",
     rows: "",
-    color: " #777"
+    color: "#777777"
   },
   {
     label: "3",
     columns: "",
     rows: "",
-    color: " #777"
+    color: "#777777"
   },
   {
     label: "+",
     columns: "",
     rows: "",
-    color: "rgba(252,156,23,1)"
+    color: "#fc9c17"
   },
   {
     label: "0",
     columns: "span 2",
     rows: "",
-    color: "#777"
+    color: "#777777"
   },
   {
     label: ".",
     columns: "",
     rows: "",
-    color: " #777"
+    color: "#777777"
   },
   {
     label: "=",
     columns: "",
     rows: "",
-    color: "rgba(252,156,23,1)"
+    color: "#fc9c17"
   }
 ];
 
